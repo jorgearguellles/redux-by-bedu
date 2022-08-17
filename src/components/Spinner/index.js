02da78163aa63b9af97fc5c902f1cfd0bae5a191
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-17 16:04:25 -0500

    add Loading Spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import './styles.css';
+
+export const LoadingSpinner = () => <div className='spinner-container'><div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>;
+
--- src/components/Users/index.js
+++ src/components/Users/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { LoadingSpinner } from "../Spinner";
 
 // 7 import
 import { connect } from 'react-redux';
@@ -7,15 +8,25 @@ import * as usersActions from '../../actions/usersActions';
 
 class Users extends Component {
 
-  // constructor(){
-  //   super()
-  //   this.state = {
-  //     users: []
-  //   }
-  // }
-
   componentDidMount(){
     this.props.getAllUsers();
+  };
+
+  renderContent = () => {
+    return(
+      <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Link</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.putRows()}
+          </tbody>
+        </table>
+    );
   };
 
   putRows = () => (
@@ -32,18 +43,7 @@ class Users extends Component {
     console.log(this.props);
     return (
       <div className="margin">
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Link</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.putRows()}
-          </tbody>
-        </table>
+        {this.props.loading ? <LoadingSpinner/> : this.renderContent()}
       </div>
     );
   };
