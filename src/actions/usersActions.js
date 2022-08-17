ddf7af080701d5df13b10c414ed7c1d2fe10c022
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-17 15:16:05 -0500

    handle error using Try-Catch in Axios get action

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -2,10 +2,14 @@ import axios from "axios";
 import { GET_ALL_USERS } from '../types/usersTypes';
 
 export const getAllUsers = () => async (dispatch) => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+    try {
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
 
-    dispatch({
-        type: GET_ALL_USERS,
-        payload: data,
-    })
+        dispatch({
+            type: GET_ALL_USERS,
+            payload: data,
+        })
+    } catch (error) {
+        console.error(error.name + ':',error.message)
+    }
 };
