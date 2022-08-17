2a696eec435dc2bdbfa70d0ff88459c008ec307e
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2022-08-17 18:57:32 -0500

    share data by actionReducers

--- src/components/Publications/index.js
+++ src/components/Publications/index.js
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import { Component } from "react";
+import { connect } from 'react-redux';
+import * as usersActions from '../../actions/usersActions';
 
 function withParams(Component) {
 	return props => <Component {...props} params={useParams()} />;
@@ -7,13 +9,25 @@ function withParams(Component) {
 
 class Publications extends Component {
 
+  componentDidMount(){
+    if(!this.props.users.length){
+      this.props.getAllUsers();
+    }
+  }
+
 	render() {			
+    console.log(this.props)
 		return (
-			<div>			
+			<div>
+        <h1>Publication of </h1>
 				{this.props.params.key}               
 			</div>
 		);
 	}
+};
+
+const mapStateToProps = (reducers) => {
+  return reducers.usersReducer;
 }
 
-export default withParams(Publications);
+export default connect(mapStateToProps, usersActions)(withParams(Publications));
